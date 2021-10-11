dec1db674ec19f1cf1b8166a8f57b1d992514f88
Fauzi Alvan <youhaveamessag3@gmail.com>
2021-10-11 18:47:38 +0700

    menambahkan box shadow .jumbo img, menambahkan overflow .jumbo, menambahkan paralax

--- js/script2.js
+++ js/script2.js
@@ -16,3 +16,21 @@ $(document).ready(function(){
       }, 800);
   });
 });
+
+
+// paralax
+$(window).scroll(function(){
+  var wScroll = $(this).scrollTop();
+
+  $('.jumbotron img').css({
+    'transform' : 'translate(0px, '+ wScroll/4 +'%)'
+  });
+
+  $('.jumbotron h1').css({
+    'transform' : 'translate(0px, '+ wScroll/2 +'%)'
+  });
+
+  $('.jumbotron p').css({
+    'transform' : 'translate(0px, '+ wScroll/2 +'%)'
+  });
+});
