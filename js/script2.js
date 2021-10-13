77981a2d41b9e2c9d1449a0bd1123313d4ad0c95
Fauzi Alvan Fadillah <youhaveamessag3@gmail.com>
2021-10-13 18:50:42 +0700

    menambahkan animasi pada paragraph

--- js/script2.js
+++ js/script2.js
@@ -1,43 +1,56 @@
-$(document).ready(function(){
+$(document).ready(function () {
   // Add smooth scrolling to all links
-  $(".page-scroll").on('click', function(event) {
+  $(".page-scroll").on('click', function (event) {
 
     // Make sure this.hash has a value before overriding default behavior
-      // Prevent default anchor click behavior
-      event.preventDefault();
+    // Prevent default anchor click behavior
+    event.preventDefault();
 
-      // Store hash
-      var hash = this.hash;
+    // Store hash
+    var hash = this.hash;
 
-      // Using jQuery's animate() method to add smooth page scroll
-      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top - 55
-      }, 800);
+    // Using jQuery's animate() method to add smooth page scroll
+    // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+    $('html, body').animate({
+      scrollTop: $(hash).offset().top - 55
+    }, 800);
   });
 });
 
 
 // paralax
-$(window).scroll(function(){
+
+// about
+$(window).on('load', function () {
+  $('.pKiri').addClass('pMuncul');
+  $('.pKanan').addClass('pMuncul');
+});
+
+$(window).scroll(function () {
 
   // jumbotron
   var wScroll = $(this).scrollTop();
 
   $('.jumbotron img').css({
-    'transform' : 'translate(0px, '+ wScroll/4 +'%)'
+    'transform': 'translate(0px, ' + wScroll / 4 + '%)'
   });
 
   $('.jumbotron h1').css({
-    'transform' : 'translate(0px, '+ wScroll/2 +'%)'
+    'transform': 'translate(0px, ' + wScroll / 2 + '%)'
   });
 
   $('.jumbotron p').css({
-    'transform' : 'translate(0px, '+ wScroll/2 +'%)'
+    'transform': 'translate(0px, ' + wScroll / 2 + '%)'
   });
 
   //portfolio
   if (wScroll > $('.portfolio').offset().top - 200) {
-    $('.portfolio .img-thumbnail').addClass('tampil');
+    $('.portfolio .img-thumbnail').each(function (i) {
+      setTimeout(function () {
+        $('.portfolio .img-thumbnail').eq(i).addClass('tampil');
+      }, 500 * (i + 1));
+    });
+
+
   }
 });
